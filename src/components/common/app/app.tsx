5c79656cf171ea8045299d2adec993d0670c4fa9
unknown <thom.florian.1997@googlemail.com>
2020-12-15 01:50:20 +0100

    Added some references

--- src/components/common/app/app.tsx
+++ src/components/common/app/app.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import './app.scss';
 
+// typescript intros
+//  https://www.sitepoint.com/react-with-typescript-best-practices/
+//  https://www.youtube.com/watch?v=Z5iWr6Srsj8&ab_channel=BenAwad
+
 function App(): React.ReactNode {
     return <div className="App">hi</div>;
 }
