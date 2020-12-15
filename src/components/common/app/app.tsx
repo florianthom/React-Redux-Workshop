0c6e8355510cfea4f00894c9d2bc1f97eccd65ab
unknown <thom.florian.1997@googlemail.com>
2020-12-15 01:26:14 +0100

    Ajusted eslint config

--- src/components/common/app/app.tsx
+++ src/components/common/app/app.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
 import './app.scss';
 
-function App() {
-  return (
-    <div className="App">
-      hi
-    </div>
-  );
+function App(): React.ReactNode {
+    return <div className="App">hi</div>;
 }
 
 export default App;
-
