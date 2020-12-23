33a833ea89d8d18f3faf32544bafe24bc910b492
unknown <thom.florian.1997@googlemail.com>
2020-12-23 01:43:55 +0100

    Renamed constants

--- src/store/reducers/counterReducer.tsx
+++ src/store/reducers/counterReducer.tsx
@@ -1,4 +1,4 @@
-import * as constants from '../constants/actionTypes';
+import * as constants from '../actionTypes/actionTypes';
 import { Actions } from '../actions';
 
 export interface CounterState {
